e48d9a05a28ca4c0d333db19e08b7425bc18352a
Mihalis Papakonstadinou <mihalispapak@gmail.com>
2016-03-09 18:16:50 +0200

    deleted:    createCollectionIndex.sh 	deleted:    data/4592.json 	deleted:    index_json.sh 	deleted:    install.sh

--- index_json.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-content="$(cat data/4592.json)"
-
-#TODO: think about given id, perhaps strip that from handle.net? seems like a good choice
-echo "curl -XPUT 'http://localhost:9201/cimmyt/collection/4592' -d '${content}'"
