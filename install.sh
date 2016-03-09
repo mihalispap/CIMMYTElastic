9b0c1fed4a71c5ad9b0cbb9583e19afb3bebd432
Mihalis Papakonstadinou <mihalispapak@gmail.com>
2016-03-09 15:32:29 +0200

    first

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.5.2.deb
+
+sudo dpkg -i elasticsearch-1.5.2.deb
+
+#CONFIGURE ES
+sudo pico /etc/elasticsearch/elasticsearch.yml
+
+sudo /usr/share/elasticsearch/bin/plugin -install mobz/elasticsearch-head
+sudo /usr/share/elasticsearch/bin/plugin -install lukas-vlcek/bigdesk
+
+sudo service elasticsearch start
