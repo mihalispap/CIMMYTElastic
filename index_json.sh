2fdffb21e125cdccf9816846451024e9800453da
Mihalis Papakonstadinou <mihalispapak@gmail.com>
2016-03-11 17:52:23 +0200

    index sample

--- index_json.sh
+++ index_json.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-content="$(cat data/4592.json)"
+content="$(cat data/cimmyt_resource3664NOLB.json)"
 
 #TODO: think about given id, perhaps strip that from handle.net? seems like a good choice
 echo "curl -XPUT 'http://localhost:9201/cimmyt/collection/4592' -d '${content}'"
